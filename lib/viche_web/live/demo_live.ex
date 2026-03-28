b56bda479e35eaa3ec4d13ddad6bc4ad11642321
Joel Milligan <joeldevelops@gmail.com>
2026-03-28 14:33:39 +0100

    feat(demo): add pulsing rings, counter pop animation, real agent count

--- lib/viche_web/live/demo_live.ex
+++ lib/viche_web/live/demo_live.ex
@@ -3,12 +3,21 @@ defmodule VicheWeb.DemoLive do
 
   @impl true
   def mount(_params, _session, socket) do
+    agents = Viche.Agents.list_agents()
+
     if connected?(socket) do
       Phoenix.PubSub.subscribe(Viche.PubSub, "demo:joins")
+      Phoenix.PubSub.subscribe(Viche.PubSub, "registry:global")
       Process.send_after(self(), :fake_join, :rand.uniform(5000) + 5000)
     end
 
-    {:ok, assign(socket, join_count: 0, qr_hash: "a8f3c2")}
+    {:ok,
+     assign(socket,
+       join_count: 0,
+       qr_hash: "a8f3c2",
+       agent_count: length(agents),
+       messages_today: 0
+     )}
   end
 
   @impl true
@@ -26,4 +35,14 @@ defmodule VicheWeb.DemoLive do
   def handle_info(%Phoenix.Socket.Broadcast{event: "new_join"}, socket) do
     {:noreply, assign(socket, join_count: socket.assigns.join_count + 1)}
   end
+
+  def handle_info(%Phoenix.Socket.Broadcast{event: "agent_joined"}, socket) do
+    {:noreply, assign(socket, agent_count: length(Viche.Agents.list_agents()))}
+  end
+
+  def handle_info(%Phoenix.Socket.Broadcast{event: "agent_left"}, socket) do
+    {:noreply, assign(socket, agent_count: length(Viche.Agents.list_agents()))}
+  end
+
+  def handle_info(_, socket), do: {:noreply, socket}
 end
